2e88440c813c52176fc05b846a308ffeb541395b
house472 <morehouse472@hotmail.com>
2020-02-17 01:36:30 -0600

    basics ic classwork

---
+++ index.js
@@ -0,0 +1,36 @@
+// Problem 1
+// Ask the user for input. Print Hello [THEIR NAME]
+
+// let name = prompt ("Enter Your Name")
+// console.log( "Hello " + name)
+
+
+// Problem 2
+// Prints “Good Afternoon!”. Ask the user “What is your name?". Print “Hello [THEIR NAME]”
+
+// let name= prompt("What is your name?")
+// console.log("Good Afernoon! ") 
+// console.log("Hello " + name)
+
+// Problem 3
+// Ask the user to enter their balance. Print "I’ve granted you $50 more!". Print "Your new balance is: [NEW BALANCE]".
+
+// let balance= prompt("Enter Your Balance")
+// let newbal= parseInt(balance)  + 50
+
+// console.log( "I've granted you $50 more! Your new balance is " + newbal)
+
+// Problem 4
+// Ask for the user to enter a number. Ask the user for another number. Print the sum of the two numbers. Print the difference of the two numbers.
+
+// let num1 = prompt("Enter a number")
+// let num2 = prompt("Enter another number")
+// console.log(parseInt (num1) + parseInt(num2))
+// console.log(num1-num2)
+
+// Problem 5
+// Create a variable to hold Tenessee's tax rate (9.25%). Ask the user "Enter a price". Print "The price after tax is: [NEW PRICE]".
+
+// let price = prompt("Enter a price")
+// let tax = price * 1.0925
+// console.log("The price after tax is " + tax)
